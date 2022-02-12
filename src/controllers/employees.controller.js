abb97885f09bb76961f6c5cbf64f5c9b87fa0905
ManuelCubillos <maccubillos30@gmail.com>
2022-02-11 23:42:46 -0500

    Fase inicial backend

---
+++ src/controllers/employees.controller.js
@@ -0,0 +1,10 @@
+const employeesCtrl = {}
+
+employeesCtrl.getEmployees = (req, res) => {}
+employeesCtrl.createEmployee = (req, res) => {}
+employeesCtrl.getEmployee = (req, res) => {}
+employeesCtrl.editEmployee = (req, res) => {}
+employeesCtrl.deleteEmployee = (req, res) => {}
+
+
+module.exports = employeesCtrl
